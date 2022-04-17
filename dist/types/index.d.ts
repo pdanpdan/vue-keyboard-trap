563e5a0f0e627328935dd115666bea3ea35ac09c
Dan Popescu <pdan.popescu@gmail.com>
2022-04-17 19:01:05 +0300

    gitpkg

---
+++ dist/types/index.d.ts
@@ -0,0 +1,11 @@
+interface IVueKeyboardTrapDirectiveOptions {
+    name?: string;
+    datasetName?: string;
+    focusableSelector?: string;
+    rovingSkipSelector?: string;
+    gridSkipSelector?: string;
+    autofocusSelector?: string;
+    trapTabIndex?: number;
+}
+
+export { IVueKeyboardTrapDirectiveOptions };
