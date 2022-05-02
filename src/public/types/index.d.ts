da13d0260c1fdccf546c335afa53c9a60b6997f8
Dan Popescu <pdan.popescu@gmail.com>
2022-05-02 21:02:44 +0300

    fix(types): add types for plugin and factory

--- src/public/types/index.d.ts
+++ src/public/types/index.d.ts
@@ -1,11 +1,20 @@
-interface IVueKeyboardTrapDirectiveOptions {
-    name?: string;
-    datasetName?: string;
-    focusableSelector?: string;
-    rovingSkipSelector?: string;
-    gridSkipSelector?: string;
-    autofocusSelector?: string;
-    trapTabIndex?: number;
+import type { App } from 'vue';
+
+export interface IVueKeyboardTrapDirectiveOptions {
+  name?: string;
+  datasetName?: string;
+  focusableSelector?: string;
+  rovingSkipSelector?: string;
+  gridSkipSelector?: string;
+  autofocusSelector?: string;
+  trapTabIndex?: number;
 }
 
-export { IVueKeyboardTrapDirectiveOptions };
+type IVueDirectivePlugin = {
+  install( app: App, options: IVueKeyboardTrapDirectiveOptions ): void,
+};
+
+export const VueKeyboardTrapDirectivePlugin: IVueDirectivePlugin;
+export function VueKeyboardTrapDirectiveFactory( options?: IVueKeyboardTrapDirectiveOptions ): { name: string, directive: object; };
+
+export default VueKeyboardTrapDirectivePlugin;
